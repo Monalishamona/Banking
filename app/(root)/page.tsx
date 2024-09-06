8add79ddcd56f8b3a042dd46a7b524907d5f0eb1
Monalisha56 <thingsofbeauty09@gmail.com>
2024-09-06 18:55:29 +0530

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,33 @@
+import { Section } from 'lucide-react'
 import React from 'react'
+// import HeaderBox  from '@/components/HeaderBox'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import RightSidebar from '@/components/RightSidebar'
 
 const Home = () => {
+  const loggedIn ={firstName: 'Monalisha',lastname : 'Thakur',email:'lishamona7369@gmail.com'}
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+          type="greeting" 
+          title="Welcome"
+          user={loggedIn?.firstName ||'Guest'}
+          subtext = "Access and manage your account and transcations efficiently."
+          />
+          <TotalBalanceBox
+            accounts ={[]}
+            totalBanks = {1}
+            totalCurrentBalance = {1250.35}
+           />
+        </header>
+        RECENT TRANSACTIONS
+          
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]}  banks = {[{currentBalance:123.50},{currentBalance:500}]}/> 
+    </section>
   )
 }
 
---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+'use client';
+import CountUp from 'react-countup'
+
+const AnimatedCounter = ({amount}:{amount:number}) => {
+  return (
+    <div className='w-full'>
+      <CountUp
+      duration={2}
+      decimal='2'
+      prefix="₹" 
+       end={amount}/>
+    </div>
+  )
+}
+
+export default AnimatedCounter
